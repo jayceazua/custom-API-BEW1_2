3d65a90d7b6ddcbb638dff4b2bfb09ae5ef1a2e8
Jayce Azua <jayce.azua@gmail.com>
2018-10-02 15:50:42 -0700

    editing my endpoints

--- controller/puppies.js
+++ controller/puppies.js
@@ -1,3 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const Puppy = require('../models/puppy');
+
+// INDEX
+router.get('', (req, res) => {
+
+});
+// NEW
+router.get('/new', (req, res) => {
+
+});
+// CREATE
+router.post('', (req, res) => {
+
+});
+// SHOW
+router.get('/:id', (req, res) => {
+
+});
+// EDIT
+router.get('/:id/edit', (req, res) => {
+
+});
+// UPDATE
+router.put('/:id', (req, res) => {
+    
+});
+// DELETE
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;
